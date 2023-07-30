d178a5752a6b93d7d9f6bdfd7863c78e46e951d6
Darren <you@example.comdarren@kandekore.net>
2023-07-30 23:27:03 +0100

    reslut convirsion

---
+++ readme.md
@@ -0,0 +1,47 @@
+# Area Calculato
+
+## Table of Contents
+
+1. [Introduction](#introduction)
+2. [Features](#features)
+3. [Demo](#demo)
+4. [Installation](#installation)
+5. [Usage](#usage)
+6. [License](#license)
+
+## Introduction
+
+The Area Calculator is a user-friendly tool that helps users quickly calculate the area of a rectangular space without the need for manual calculations. It supports multiple unit options, making it versatile and suitable for various use cases.
+
+## Features
+
+- Calculate area in square meters based on user inputs for length and width.
+- Support for multiple unit options: centimeters, feet, inches, and meters.
+- Responsive design, ensuring it works well on different devices and screen sizes.
+
+## Demo
+
+You can try out the live demo of the application [here](https://example.com/area-calculator-demo).
+
+![Area Calculator Demo Gif](demo.gif)
+
+## Installation
+
+To use the Area Calculator locally, follow these steps:
+
+1. Clone the repository: `git clone https://github.com/yourusername/area-calculator.git`
+2. Navigate to the project directory: `cd area-calculator`
+3. Open the `index.html` file in your web browser.
+
+## Usage
+
+1. Enter the length of the rectangular space in the "Length" input box.
+2. Select the unit of measurement for the length from the dropdown menu.
+3. Enter the width of the rectangular space in the "Width" input box.
+4. Select the unit of measurement for the width from the dropdown menu.
+5. Click the "Calculate Area" button to see the result.
+6. The calculated area in square meters will be displayed below the button.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-function convertToMeters(value, unit) {
+function convertToSquareMeters(length, width, lengthUnit, widthUnit) {
+  // Conversion factors (1 meter = x units)
   const conversionFactors = {
-    cm: 0.01,
-    ft: 0.3048,
-    in: 0.0254,
+    cm: 0.0001,
+    ft: 0.09290304,
+    in: 0.00064516,
     m: 1
   };
 
-  return value * conversionFactors[unit];
+  const lengthInMeters = length * conversionFactors[lengthUnit];
+  const widthInMeters = width * conversionFactors[widthUnit];
+
+  return lengthInMeters * widthInMeters;
 }
 
 function calculateArea() {
@@ -14,13 +18,34 @@ function calculateArea() {
   const width = parseFloat(document.getElementById("width").value);
   const lengthUnit = document.getElementById("lengthUnit").value;
   const widthUnit = document.getElementById("widthUnit").value;
+  const resultUnit = document.getElementById("resultUnit").value;
 
-  const lengthInMeters = convertToMeters(length, lengthUnit);
-  const widthInMeters = convertToMeters(width, widthUnit);
+  const areaInSquareMeters = convertToSquareMeters(
+    length,
+    width,
+    lengthUnit,
+    widthUnit
+  );
 
-  const areaInSquareMeters = lengthInMeters * widthInMeters;
+  const resultFactors = {
+    cm: 1000,
+    ft: 3.28084,
+    in: 0.00064516,
+    m: 1
+  };
+
+  // Conversion factors to the selected result unit
+  const resultConversionFactors = {
+    m2: 1,
+    ft2: 10.7639, // 1 square meter = 10.7639 square feet
+    in2: 1550.0031, // 1 square meter = 1550.0031 square inches
+    cm2: 10000 // 1 square meter = 10000 square centimeters
+  };
+
+  const areaInResultUnit =
+    areaInSquareMeters * resultConversionFactors[resultUnit];
 
   document.getElementById(
     "result"
-  ).textContent = `Area: ${areaInSquareMeters.toFixed(2)} square meters`;
+  ).textContent = `Area: ${areaInResultUnit.toFixed(2)} ${resultUnit}`;
 }
